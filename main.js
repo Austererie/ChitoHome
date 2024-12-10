453b893011aea71b0587b83c6b6f39001b7448d2
Unique Tegegn G. <143523948+Austererie@users.noreply.github.com>
2024-12-10 19:39:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlists');
-let close= document.querySelector('#close-icon');
-
-menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navlist.classList.toggle('open');
-}
-
 const sr = ScrollReveal ({
   distance: '65px',
   duration: 2600,
